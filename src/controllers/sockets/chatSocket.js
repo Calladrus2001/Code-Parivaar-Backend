a5063be2b43d92310eaf81e1c7610a631c9a8ab2
Calladrus2001 <dugarvishesh@gmail.com>
2023-09-02 21:03:01 +0530

    feat: chatSocket implemented

--- src/controllers/sockets/chatSocket.js
+++ src/controllers/sockets/chatSocket.js
@@ -1,10 +1,28 @@
 module.exports = (io) => {
+  
+  const onlineUsers = new Map();
+
   io.on('connection', (socket) => {
-    console.log("New chat connection");
+    const email = socket.handshake.headers.email;
+    onlineUsers.set(socket.id, email);
+    console.log(`${socket.id} connected as ${email}`)
     //TODO: check for pending messages
 
     socket.on('message', (message) => {
-      console.log(message.sender);
+      const groupID = message.groupID;
+      io.to(groupID).emit(`${groupID}`, message);
+      console.log(message);
+    });
+
+    socket.on('config', (config) => {
+      const groupID = config.groupID;
+      socket.join(groupID);
+      console.log(`${socket.id} has joined ${config.groupID}`)
+    });
+
+    socket.on("disconnect", () => {
+      onlineUsers.delete(socket.id);
+      console.log(`${socket.id} disconnected`);
     });
   });
 }
